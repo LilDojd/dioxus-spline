6ed4349843127f1f4438c7015b149b3e9c22e03d
LilDojd <yawner@protonmail.com>
2024-05-04 11:03:56 +0400

    feat(example): Update example

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,7 +1,7 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
-use dioxus_spline::{Application, SPEObject, SPEVector3, Spline, SplineEvent};
+use dioxus_spline::{SPEObject, SPEVector3, Spline, SplineApplication, SplineEvent};
 use tracing::Level;
 
 fn main() {
@@ -12,22 +12,27 @@ fn main() {
 pub fn App() -> Element {
     let mut splineobject = use_signal(|| None::<SPEObject>);
     let mut number_events_received = use_signal(|| 0);
+    let mut target = use_signal(|| String::from("None"));
     rsx! {
         div { width: "800px",
             Spline {
                 scene: String::from("https://prod.spline.design/PWOr9wT1pcAkbAA7/scene.splinecode"),
-                on_load: move |event: Application| {
-                    let obj = event.findObjectByName(String::from("Helix 2"));
+                on_load: move |event: SplineApplication| {
+                    let obj = event.find_object_by_name(String::from("Helix 2"));
                     let _ = splineobject.write().insert(SPEObject::new(obj));
                     tracing::info!("object: {splineobject:?}");
                 },
                 on_mouse_down: move |event: SplineEvent| {
                     tracing::info!("Recieved {event:?}");
                     number_events_received += 1;
+                    let recent_target = event.target.name;
+                    target.set(recent_target);
                 },
                 on_mouse_hover: move |event: SplineEvent| {
                     tracing::info!("Mouse hover event {event:?}");
                     number_events_received += 1;
+                    let recent_target = event.target.name;
+                    target.set(recent_target);
                 }
             }
         }
@@ -41,6 +46,6 @@ pub fn App() -> Element {
             "Make helix chonky!"
         }
         h1 { "Received {number_events_received} Spline events. (Click on blue helix/hover over text)" }
+        p { "Most recent target: {target} "}
     }
 }
-
